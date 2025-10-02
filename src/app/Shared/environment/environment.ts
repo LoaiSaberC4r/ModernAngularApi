3da1cc8e253936e9d22e3eeba1b7f01a375bbca8
mahmoudniletronix <mahmoudniletronix@gmail.com>
2025-10-02 18:36:42 +0300

    dir

--- src/app/Shared/environment/environment.ts
+++ src/app/Shared/environment/environment.ts
@@ -1,4 +1,4 @@
 export class environment {
-  public static baseUrl = 'http://localhost:80/api/v1';
-  public static signalrUrl = 'http://localhost:80/signalr';
+  public static baseUrl = 'http://192.168.1.100:80/api/v1';
+  public static signalrUrl = 'http://192.168.1.100:80/signalr';
 }
